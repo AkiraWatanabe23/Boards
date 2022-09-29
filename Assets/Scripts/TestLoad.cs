33a22594023bd85a36b0f36b68f305e28590d36f
Akira Watanabe <a0323w@gmail.com>
2022-09-29 23:51:05 +0900

    ファイルロードについて理解する

--- Assets/Scripts/TestLoad.cs
+++ Assets/Scripts/TestLoad.cs
@@ -8,26 +8,27 @@ public class TestLoad : MonoBehaviour
 {
     const int BOARD_HEIGHT = 8;
     const int BOARD_WIDTH = 8;
-    [SerializeField] string[][] _tile = new string[BOARD_HEIGHT][]; //ジャグ配列を宣言
-
+    //ジャグ配列を宣言
+    [SerializeField] string[][] _tile = new string[BOARD_HEIGHT][];
     public string[][] Tile { get => _tile; set => _tile = value; }
 
     // Start is called before the first frame update
     void Start()
     {
         // Addressables Assets Systemを利用し、Addressables Groupから
-        // 読み込む対象のパスを指定し、アセットを読み込む。
+        // 読み込む対象のパスを指定し、アセットを読み込む(アセット名をstringで指定)
         Addressables.LoadAssetAsync<TextAsset>("Assets/initial placement.csv").Completed +=
             // 読み込んだアセット(csv)をコンソールに表示する。
             (a) =>
             {
                 Debug.Log(a);
-                Debug.Log($"{a.Result}");
+                Debug.Log($"{a.Result}"); //a.Result...読み込んだ内容全体
 
                 var sr = new StringReader(a.Result.text);
                 string value = "";
                 bool isFirstLine = true;
                 int count = 0;
+                //読み込んだ情報がnullで無ければ、以下の処理を行う
                 while ((value = sr.ReadLine()) != null)
                 {
                     if (isFirstLine)
@@ -36,17 +37,18 @@ public class TestLoad : MonoBehaviour
                         continue;
                     }
                     Tile[count] = value.Split(',');
+                    Debug.Log(count);
 
                     count++;
-
-                    Debug.Log(value);
+                    Debug.Log(value); //1行ごとの入力(2行目以降)
                 }
             };
 
         for (int i = 0; i < Tile.Length; i++)
         {
             Tile[i] = new string[BOARD_WIDTH];
-            Debug.Log(i);
+            print($"{i}番目の配列の要素数は {Tile[i].Length} です");
+            //Debug.Log(Tile[i].Length); //ジャグ配列の要素数を確認
         }
     }
 
@@ -64,5 +66,22 @@ public class TestLoad : MonoBehaviour
     void Update()
     {
 
+    }
+
+    enum TileState
+    {
+        B_King = -6,
+        B_Queen,
+        B_Rook,
+        B_Bishop,
+        B_Knight,
+        B_Pawn,
+        None = 0,
+        W_Pawn,
+        W_Knight,
+        W_Bishop,
+        W_Rook,
+        W_Queen,
+        W_King,
     }
 }
