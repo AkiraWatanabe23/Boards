e2baa8a525dfd34a1c4e1c763bfeee69c7a314ee
Akira Watanabe <a0323w@gmail.com>
2022-09-29 21:46:50 +0900

    読み込んだ情報をそれぞれのマスに割り当てたい

--- Assets/Scripts/TestLoad.cs
+++ Assets/Scripts/TestLoad.cs
@@ -6,10 +6,19 @@ using UnityEngine.AddressableAssets;
 
 public class TestLoad : MonoBehaviour
 {
+    const int BOARD_HEIGHT = 8;
+    const int BOARD_WIDTH = 8;
+    [SerializeField] string[][] _tile = new string[BOARD_HEIGHT][]; //ジャグ配列を宣言
+
+    public string[][] Tile { get => _tile; set => _tile = value; }
+
     // Start is called before the first frame update
     void Start()
     {
+        // Addressables Assets Systemを利用し、Addressables Groupから
+        // 読み込む対象のパスを指定し、アセットを読み込む。
         Addressables.LoadAssetAsync<TextAsset>("Assets/initial placement.csv").Completed +=
+            // 読み込んだアセット(csv)をコンソールに表示する。
             (a) =>
             {
                 Debug.Log(a);
@@ -18,6 +27,7 @@ public class TestLoad : MonoBehaviour
                 var sr = new StringReader(a.Result.text);
                 string value = "";
                 bool isFirstLine = true;
+                int count = 0;
                 while ((value = sr.ReadLine()) != null)
                 {
                     if (isFirstLine)
@@ -25,13 +35,19 @@ public class TestLoad : MonoBehaviour
                         isFirstLine = false;
                         continue;
                     }
-                    // string[] values = value.Split(',');
+                    Tile[count] = value.Split(',');
+
+                    count++;
 
                     Debug.Log(value);
                 }
             };
 
-
+        for (int i = 0; i < Tile.Length; i++)
+        {
+            Tile[i] = new string[BOARD_WIDTH];
+            Debug.Log(i);
+        }
     }
 
     void OnLoadCsv(TextAsset csv)
