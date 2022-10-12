3802c743f277a104edfbc820ccdc0e60ff465090
Akira Watanabe <a0323w@gmail.com>
2022-10-12 20:04:58 +0900

    選択中の駒の取得、コメント追加

--- Assets/Scripts/PieceManager.cs
+++ Assets/Scripts/PieceManager.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PieceManager : MonoBehaviour
 {
     [SerializeField] Material _select;
+    [SerializeField] GameObject _selectPiece;
     bool[,] _movable = new bool[8, 8];
+    //駒を選んだ時に色を変える
     public Material Select { get => _select; set => _select = value; }
+    //現在選ばれている駒
+    public GameObject SelectPiece { get => _selectPiece; set => _selectPiece = value; }
+    //駒に割り振った番号(個別探索処理に使う)
     public int PieceNum { get; set; }
     //選択した駒のマス番号を取得する(X,Z)
     public int TileNumX { get; set; }
     public int TileNumZ { get; set; }
     //マスの移動可、不可を判断するのに使う予定(trueなら動ける,獲れる、falseなら出来ないのようなイメージ)
     public bool[,] Movable { get => _movable; set => _movable = value; }
-    public GameObject SelectPiece { get; set; }
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +28,9 @@ public class PieceManager : MonoBehaviour
     //駒の個別移動処理への遷移
     public void PieceMovement()
     {
+        SearchReset();
         //ここで、一旦全ての駒の探索をリセットする処理を書く(?)
         //↑それぞれのMovement()に書くと、駒を変えた時に呼ばれないため
-        SearchReset();
 
         switch (Mathf.Abs(PieceNum))
         {
